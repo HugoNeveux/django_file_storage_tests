2f8975d14d6167ae71d5b1132350225cac3c8631
HugoNeveux <hugoneveux@orange.fr>
2020-02-10 11:22:48 +0100

    Added upload view

--- Django_webdav_tests/Files_list/models.py
+++ Django_webdav_tests/Files_list/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UserFile(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField()
+
+    def __str__(self):
+        return self.name
--- Django_webdav_tests/Files_list/views.py
+++ Django_webdav_tests/Files_list/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import UploadFileForm
+from .models import UserFile
 
-# Create your views here.
+def upload_file(request):
+    save = False
+    form = UploadFileForm(request.POST or None, request.FILES)
+    if form.is_valid():
+        f = UserFile()
+        f.name = form.cleaned_data['name']
+        f.file = form.cleaned_data['file']
+        f.save()
+        save = True
+
+    return render(request, 'upload.html', {
+        'form': form,
+        'sauvegarde': save
+    })
