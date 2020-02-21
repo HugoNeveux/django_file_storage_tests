9b5f87038063b53c09fce8469c4d933c91e146ee
HugoNeveux <hugoneveux@orange.fr>
2020-02-21 17:29:48 +0100

    Changed login url to app root

--- Django_webdav_tests/Django_webdav_tests/settings.py
+++ Django_webdav_tests/Django_webdav_tests/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Users settings
+LOGIN_REDIRECT_URL = "files"
--- Django_webdav_tests/Files_list/urls.py
+++ Django_webdav_tests/Files_list/urls.py
@@ -6,5 +6,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^files/(?P<path>.*)$', views.files, name='files'),
     url(r'download/(?P<path>.*)$', views.download, name='download'),
-    url(r'^login/', auth_views.LoginView.as_view())
+    url(r'^$', auth_views.LoginView.as_view())
 ]
