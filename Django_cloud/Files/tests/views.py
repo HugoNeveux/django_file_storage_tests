ddb0e2cdb32b1b9078260dae84896c8f5b01a0ed
HugoNeveux <hugoneveux@orange.fr>
2020-05-13 23:09:35 +0200

    Add some tests and delete useless view

--- Django_cloud/Files/tests/views.py
+++ Django_cloud/Files/tests/views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from Files.models import UserFile
+from Auth.models import Profile
 import os
 import shutil
 
@@ -22,16 +23,31 @@ class UploadFileTest(TestCase):
         r = self.client.post('/Files/tree/', follow=True)
         self.assertRedirects(r, '/?next=/Files/tree/')
 
-    # def test_files_upload_success(self):
-    #     self.client.login(username='temporary', password='temporary')
-    #     for file in os.listdir('./Files/tests/upload_f'):
-    #         with open(f'./Files/tests/upload_f/{file}') as f:
-    #             response = self.client.post(
-    #                 '/Files/tree/', {'name': f.name, 'file': f})
-    #             print(UserFile.objects.all())
-    #             self.assertEqual(UserFile.objects.filter(name=f.name).count(), 1)
+    def test_files_upload_success(self):
+        self.client.login(username='temporary', password='temporary')
+        for file in os.listdir('./Files/tests/upload_f'):
+            with open(f'./Files/tests/upload_f/{file}') as f:
+                response = self.client.post(
+                    '/Files/tree/', {'file': f, 'path': ''})
+                self.assertEqual(UserFile.objects.filter(name=os.path.basename(f.name)).count(), 1)
+                self.assertEqual(response.status_code, 200)
+                self.assertJSONEqual(response.content, {'form': True})
 
-    # NOTE: above test doesn't work as expected and needs fixing
+    def test_file_too_big(self):
+        user = User.objects.get(username='temporary')
+        profile = Profile.objects.get(user=user)
+        profile.upload_limit = 4*1024
+        profile.save()
+        self.client.login(username='temporary', password='temporary')
+        with open(f'./Files/tests/upload_f_err/too_big.txt') as f:
+            response = self.client.post(
+                '/Files/tree/', {'file': f, 'path': ''}
+            )
+            self.assertEqual(response.status_code, 500)
+            self.assertJSONEqual(response.content, {
+                'error': f'Limite de stockage dépassée: le fichier too_big.txt ne peut pas être enregistré.'
+            })
+            self.assertEqual(UserFile.objects.filter(name='too_big.txt').count(), 0)
 
 
 class FileListTest(TestCase):
--- Django_cloud/Files/views.py
+++ Django_cloud/Files/views.py
@@ -22,95 +22,13 @@ from io import BytesIO
 import zipfile
 
 
-@login_required
-def tree(request, path=""):
-    """Main view
-    * File reception (javascript upload)
-    * File list (showing uploaded files / folder navigation)"""
-    # Variables and file storage initialisation
-    current_dir = os.path.join(request.user.username, "files", path)
-    absolute_path = os.path.join(settings.MEDIA_ROOT, current_dir)
-    files = []
-    directories = []
-    upload_error = False
-
-    # Space available
-    user_profile = Profile.objects.get(user=request.user.id)
-    space = {"available": format_bytes(user_profile.upload_limit),
-            "used": format_bytes(user_profile.total_used),
-            "available_b": user_profile.upload_limit - user_profile.total_used }
-
-    # Upload
-    form = UploadFileForm(request.POST or None, request.FILES)
-
-    if form.is_valid():
-        existing_file = UserFile.objects.filter(
-            directory=current_dir, owner=request.user.id, name=request.FILES['file'].name)
-        if existing_file.count():
-            old_size = existing_file[0].size
-            new_size = existing_file[0].size + (old_size - form.cleaned_data["file"].size)
-            existing_file[0].file = form.cleaned_data["file"]
-            existing_file[0].size == new_size
-            user_profile.total_used += old_size - form.cleaned_data["file"].size
-            if user_profile.total_used <= user_profile.upload_limit:
-                user_profile.save()
-                existing_file[0].save(current_dir)
-        else:
-            file = UserFile(file=form.cleaned_data["file"],
-                            name=form.cleaned_data["file"].name,
-                            owner=request.user,
-                            directory=current_dir,
-                            size=form.cleaned_data["file"].size)
-            user_profile.total_used += form.cleaned_data["file"].size
-            if user_profile.total_used <= user_profile.upload_limit:
-                user_profile.save()
-                file.save(os.path.join(current_dir))
-
-
-    # Showing directory content
-    files = UserFile.objects.filter(
-        directory=current_dir, owner=request.user.id)
-
-    # Json file list
-    tmp_json = serializers.serialize("json", files)
-    files_json = json.dumps(json.loads(tmp_json))
-    directories_names = [dir for dir in os.listdir(
-        absolute_path) if os.path.isdir(os.path.join(absolute_path, dir))]
-    for name in directories_names:
-        directories.append({'name': name, 'url': os.path.join(path, name)})
-
-
-    # Breadcrumb
-    breadcrumb = {}
-    full_path = path.replace("/", "\\").split("\\")
-    breadcrumb["path"] = []
-    to_dir = ""
-    for dir in full_path[:-1]:
-        to_dir = os.path.join(to_dir, dir)
-        breadcrumb["path"].append([dir, to_dir])
-
-    breadcrumb["active"] = full_path[-1]
-    # Showing web page & rendering template
-    return render(request, 'files.html', {
-        'form': form,
-        'directory_files': files,
-        'directory_directories': directories,
-        'breadcrumb': breadcrumb,
-        'files_json': files_json,
-        'current_dir': path,
-        'space': space,
-        'upload_error': upload_error,
-        'user': request.user,
-    })
-
 class FileUploadAndListView(LoginRequiredMixin, FormView):
     """Read ans save sent file"""
     form_class = UploadFileForm
     template_name = 'files.html'
 
-    def post(self, request, path, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         path = request.POST.get('path', None)
-        print(path)
         # Variables and file storage initialisation
         current_dir = os.path.join(request.user.username, "files", path)
         absolute_path = os.path.join(settings.MEDIA_ROOT, current_dir)
@@ -152,7 +70,8 @@ class FileUploadAndListView(LoginRequiredMixin, FormView):
 
             return JsonResponse({'form': True})
         else:
-            return JsonResponse({'form': False})
+            return JsonResponse({'error': 'Une erreur inconnue s\'est produite. Merci de contacter un administrateur.'}, status=500)
+
 
 
     def get(self, request, path='', *args, **kwargs):
